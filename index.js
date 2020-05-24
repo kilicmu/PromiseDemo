4dedc8341e29c4a3c8efb7700804304abb8a0231
kilicmu <kilicmu3389@outlook.com>
2020-05-24 22:26:55 +0800

    挂defer用来测试

--- index.js
+++ index.js
@@ -130,4 +130,13 @@ class Promise {
   }
 }
 
+Promise.defer = Promise.deferred = function () {
+  let dfd = {}
+  dfd.promise = new Promise((resolve, reject) => {
+    dfd.resolve = resolve
+    dfd.reject = reject
+  })
+  return dfd
+}
+
 module.exports = Promise
