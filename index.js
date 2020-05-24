0c5670ec2027ad2dca7eaecab7cf66cc7e7c3fad
kilicmu <kilicmu3389@outlook.com>
2020-05-24 18:34:21 +0800

    处理兼容异步的resolve/reject

--- index.js
+++ index.js
@@ -1,34 +1,70 @@
+const STATE = {
+  PENDING: 'PENDING',
+  FULFILLED: 'FULFILLED',
+  REJECTED: 'REJECTED',
+}
 class Promise {
-  constructor(execture) {
-    this.status = 'pending'
+  constructor(execute) {
+    this.status = STATE.PENDING
     this.value = null
     this.reason = null
+    this.fulfilledCbs = []
+    this.rejectedCbs = []
+
     let resolve = (value) => {
-      if (this.status === 'pending') {
+      if (this.status === STATE.PENDING) {
         this.value = value
-        this.status = 'fulfilled'
+        this.status = STATE.FULFILLED
+        this.fulfilledCbs.forEach((_i) => _i())
       }
     }
 
     let reject = (err) => {
-      if (this.status === 'pending') {
-        this.value = err
-        this.status = 'rejected'
+      if (this.status === STATE.PENDING) {
+        this.reason = err
+        this.status = STATE.REJECTED
+        this.rejectedCbs.forEach((_i) => _i())
       }
     }
 
     try {
-      execture(resolve, reject)
+      execute(resolve, reject)
     } catch (e) {
       reject(e)
     }
   }
 
-  then(fulfillCb, rejectCb) {
-    if ((this.status = 'fulfillCb')) {
-      fulfillCb(this.value)
-    } else {
-      rejectCb(this.value)
+  then(onFulfilled, onRejected) {
+    /** onFulfilled和onRejected都是可选参数 */
+    if (typeof onFulfilled !== 'function') {
+      onFulfilled = () => this.value
     }
+    if (typeof onRejected !== 'function') {
+      onRejected = () => {
+        throw this.reason
+      }
+    }
+    // const _promise = new Promise((resolve, reject) => {
+    if (this.status === STATE.FULFILLED) {
+      onFulfilled(this.value)
+    } else if (this.status === STATE.REJECTED) {
+      onRejected(this.reason)
+    } else {
+      // 处理异步resolve
+      this.fulfilledCbs.push(() => onFulfilled(this.value))
+      this.rejectedCbs.push(() => onRejected(this.reason))
+    }
+    // })
+
+    // return _promise
   }
 }
+
+let p1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(1)
+  }, 1000)
+})
+p1.then((d) => {
+  console.log(d)
+})
